b75eddfea9019135d59e80bb35419189b25ccd0a
Volodymyr Sichinava <volodymyrsichinava@gmail.com>
2021-03-27 23:11:42 +0200

    Added blogs block.

--- src/js/components/filterBlogs.js
+++ src/js/components/filterBlogs.js
@@ -13,22 +13,3 @@ $(document).ready(function() {
     
   });
 })
-
-$(document).ready(function() {
-  $('.list').click(function() {
-    const value = $(this).attr('data-filter');
-    if (value == 'all') {
-      $('.itemBox').show("slow");
-    } else {
-      $('.itemBox').not('.' + value).hide("slow");
-      $('.itemBox').filter('.' + value).show("slow");
-    }
-  });
-
-  $('.list').click(function() {
-    $(this).addClass('active').siblings().removeClass('active');
-  });
-})
-
-
-//# sourceMappingURL=main.js.map
